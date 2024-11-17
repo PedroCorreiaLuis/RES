da985416852a221c37a23e2f4b5be23be0fb6b5e
pedrocorreialuis <pedro.m.f.c.correialuis@hotmail.com>
2024-11-17 23:10:56 +0000

    Abstract and fix driver

--- src/scrappers/driver.rs
+++ src/scrappers/driver.rs
@@ -7,11 +7,20 @@ use thirtyfour::{DesiredCapabilities, WebDriver};
 pub async fn initialize_driver() -> Result<WebDriver, WebDriverError> {
     let driver_path: &str =
         &*env::var("DRIVER_PATH").expect("env variable `DRIVER_PATH` should be set");
-    Command::new(driver_path).arg("--port=53466").spawn()?;
+    Command::new(driver_path).arg("--port=53465").spawn()?;
 
     tokio::time::sleep(Duration::from_secs(1)).await;
-    let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:53466", caps).await?;
+
+    let driver: WebDriver = match driver_path {
+        path if path.contains("chromedriver") => {
+            WebDriver::new("http://localhost:53465", DesiredCapabilities::chrome()).await?
+        }
+        path if path.contains("safaridriver") => {
+            WebDriver::new("http://localhost:53465", DesiredCapabilities::safari()).await?
+        }
+        _ => panic!("Unsupported driver path"),
+    };
+
     driver.maximize_window().await?;
     Ok(driver)
 }
